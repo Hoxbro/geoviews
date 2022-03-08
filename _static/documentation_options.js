90ce73e3e6e4e9d4b0c5e8b2e421f1dc3f76a641
philippjfr <philippjfr@users.noreply.github.com>
2022-03-08 15:51:43 +0000

    deploy: 5eb9cd491590335033c748f2e8c1ca388ba956da

---
+++ _static/documentation_options.js
@@ -0,0 +1,12 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.9.5+g5eb9cd4-dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: true
+};
